dcadb5a3002e38e98e83517055a6f4775a4d152e
axelriveroc <riverocoronelaxel@gmail.com>
2023-09-10 13:20:42 -0300

    pasando al ingles

--- src/components/pages/aboutMe/AboutMe.jsx
+++ src/components/pages/aboutMe/AboutMe.jsx
@@ -2,35 +2,35 @@ import "./AboutMe.css";
 import yo from "../../../assets/yo1.png";
 
 const AboutMe = () => {
-  return (
-    <section id="AboutMe" className="section_aboutMe">
-      <div className="box_aboutMe">
-        <div className="box_img_aboutMe">
-          <img src={yo} alt="yo" className="foto_aboutMe" />
-        </div>
-        <div className="box_h2_description_aboutMe">
-          <h2>About Me</h2>
-          <div className="text_description_aboutMe">
-            <p>
-              Soy desarrollador web full stack especializado en el desarrollo
-              frontend. Proveniente del campo de la psicología y la educación.
-            </p>
-            <p>
-              He trabajado en equipo para idear soluciones de mejora de un
-              producto y desarrollar webs consistentes. Soy un investigador
-              incansable y siempre estoy buscando nuevas formas de mejorar mis
-              habilidades.
-            </p>
-            <p>
-              Mi enfoque se centra en fortalecer mi habilidad de comunicación
-              efectiva, agilidad en el aprendizaje y capacidad constante para
-              resolver problemas de manera creativa.
-            </p>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section id="AboutMe" className="section_aboutMe">
+			<div className="box_aboutMe">
+				<div className="box_img_aboutMe">
+					<img src={yo} alt="yo" className="foto_aboutMe" />
+				</div>
+				<div className="box_h2_description_aboutMe">
+					<h2>About Me</h2>
+					<div className="text_description_aboutMe">
+						<p>
+							I am a full-stack web developer specializing in frontend
+							development. Coming from a background in psychology and education
+						</p>
+						<p>
+							Collaborating within a team, we have worked to brainstorm
+							improvement solutions for a product and create consistent
+							websites. My dedication to research and continuous pursuit of
+							skill enhancement is relentless.
+						</p>
+						<p>
+							My focus revolves around strengthening my ability for effective
+							communication, agility in learning, and a constant capacity for
+							creatively solving problems.
+						</p>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default AboutMe;
