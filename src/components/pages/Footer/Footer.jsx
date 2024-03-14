41f7561a37655d06664ae524afc87ec8493f4d8d
axelriveroc <riverocoronelaxel@gmail.com>
2024-03-14 19:28:10 -0300

    sasa

--- src/components/pages/Footer/Footer.jsx
+++ src/components/pages/Footer/Footer.jsx
@@ -1,6 +1,17 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   return (
     <div>
+      <ul>
+        <li>
+          {" "}
+          <Link to="/aviso_legal">Aviso Legal</Link>
+        </li>
+        <li>
+          <Link to="/privacy_policy">Política de Privacidad</Link>
+        </li>
+      </ul>
       <h5 style={{ color: "white" }}>
         Esta web es propiedad privada de Axel Rivero
       </h5>
