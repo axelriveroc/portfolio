dbe45376b14dd10af5088f5afbdb3859a5d51128
axelriveroc <riverocoronelaxel@gmail.com>
2024-03-23 09:41:56 -0300

    nombre comercial

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -2,38 +2,39 @@ import "./homeStyles.css";
 /* import img_avatar from "../../../assets/avatar1.png"; */
 import { Box } from "@mui/material";
 const Home = () => {
-	return (
-		<section id="Home" className="section_home">
-			<Box className="container_front_welcome">
-				<div className="frontend-box">
-					<h1 className="h1-name">AXEL RIVERO CORONEL</h1>
-				</div>
-				<div className="box-welcome">
-					<span>F</span>
-					<span>U</span>
-					<span>L</span>
-					<span>L</span>
-					<span>S</span>
-					<span>T</span>
-					<span>A</span>
-					<span>C</span>
-					<span style={{ paddingRight: "5px" }}>K</span>
-					<span>D</span>
-					<span>E</span>
-					<span>V</span>
-					<span>E</span>
-					<span>L</span>
-					<span>O</span>
-					<span>P</span>
-					<span>E</span>
-					<span>R</span>
-				</div>
-			</Box>
-			{/* <div>
+  return (
+    <section id="Home" className="section_home">
+      <Box className="container_front_welcome">
+        <div className="frontend-box">
+          <h1 className="h1-name">AXELRIVEROC</h1>
+          <h2 className="h1-name">Axel Rivero Coronel</h2>
+        </div>
+        <div className="box-welcome">
+          <span>F</span>
+          <span>U</span>
+          <span>L</span>
+          <span>L</span>
+          <span>S</span>
+          <span>T</span>
+          <span>A</span>
+          <span>C</span>
+          <span style={{ paddingRight: "5px" }}>K</span>
+          <span>D</span>
+          <span>E</span>
+          <span>V</span>
+          <span>E</span>
+          <span>L</span>
+          <span>O</span>
+          <span>P</span>
+          <span>E</span>
+          <span>R</span>
+        </div>
+      </Box>
+      {/* <div>
         <img src={img_avatar} alt="s" className={`img_avatar`} />
       </div> */}
-		</section>
-	);
+    </section>
+  );
 };
 
 export default Home;
